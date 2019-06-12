d4ad19535a4921ffc64153a19429d7b1b09cfad2
陈明 <1106934930@qq.com>
2019-06-12 21:32:43 +0800

    下班提交下班

--- Git_Spring/src/main/java/com/itheima/Test/UserSpringTest.java
+++ Git_Spring/src/main/java/com/itheima/Test/UserSpringTest.java
@@ -7,12 +7,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class UserSpringTest {
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void test1(){
-        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ClassPathXmlApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
         UserDao userDao1 = (UserDao) app.getBean("userDao");
         UserDao userDao2 = (UserDao) app.getBean("userDao");
         System.out.println(userDao1);
         System.out.println(userDao2);
+
+        app.close();
     }
 }
--- Git_Spring/src/main/java/com/itheima/dao/Impl/UserDaoImpl.java
+++ Git_Spring/src/main/java/com/itheima/dao/Impl/UserDaoImpl.java
@@ -6,4 +6,10 @@ public class UserDaoImpl implements UserDao {
     public void save() {
         System.out.println("save");
     }
+
+    public void init(){
+        System.out.println("初始化方法....");
+    }
+    public void destory(){        System.out.println("销毁方法.....");
+    }
 }
