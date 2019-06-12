7c504680a6d95cb9d3298d382c46e73d85bfb6e7
陈明 <1106934930@qq.com>
2019-06-12 20:42:05 +0800

    Spring 测试

---
+++ Git_Spring/src/main/java/com/itheima/dao/Impl/UserDaoImpl.java
@@ -0,0 +1,9 @@
+package com.itheima.dao.Impl;
+
+import com.itheima.dao.UserDao;
+
+public class UserDaoImpl implements UserDao {
+    public void save() {
+        System.out.println("save");
+    }
+}
